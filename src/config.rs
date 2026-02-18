9f0eba082f2ae31922090d440155164a9bc66565
atlasgorn <atlasgorn@gmail.com>
2026-02-18 18:10:57 +0300

    config default; autocreation on gen

--- src/config.rs
+++ src/config.rs
@@ -7,3 +7,15 @@ pub struct Config {
     pub dry_run: bool,
     pub backup_dir: String,
 }
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            default_category: "uncat".to_string(),
+            package_manager: "paru".into(),
+            pacman_log_file: "/var/log/pacman.log".into(),
+            dry_run: false,
+            backup_dir: ".backups".into(),
+        }
+    }
+}
